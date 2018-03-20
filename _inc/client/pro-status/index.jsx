c6e37b22f6f770b4bb97e2f6ccc2de30b59009b1
Elio Rivero <eliorivero@gmail.com>
2018-03-20 16:53:36 -0300

    Dashboard - Settings: move functions outside of component, replace multiple if with switch, consolidate and reduce expressions.

--- _inc/client/pro-status/index.jsx
+++ _inc/client/pro-status/index.jsx
@@ -39,6 +39,30 @@ import {
 	isFetchingAkismetData
 } from 'state/at-a-glance';
 
+/**
+ * Track click on Pro status badge.
+ *
+ * @param {string} type    Status of a certain feature.
+ * @param {string} feature Slug of plugin or service.
+ *
+ * @returns {undefined}
+ */
+const trackProStatusClick = ( type, feature ) => analytics.tracks.recordJetpackClick( {
+	target: 'pro-status',
+	type: type,
+	feature: feature
+} );
+
+/**
+ * Build function to pass as onClick property.
+ *
+ * @param {string} type    Status of a certain feature.
+ * @param {string} feature Slug of plugin or service.
+ *
+ * @returns {function} Function to track a click.
+ */
+const handleClickForTracking = ( type, feature ) => ( () => trackProStatusClick( type, feature ) );
+
 class ProStatus extends React.Component {
 	static propTypes = {
 		isCompact: PropTypes.bool,
@@ -48,18 +72,6 @@ class ProStatus extends React.Component {
 	static defaultProps = {
 		isCompact: true,
 		proFeature: ''
-	};
-
-	trackProStatusClick = ( type, feature ) => {
-		analytics.tracks.recordJetpackClick( {
-			target: 'pro-status',
-			type: type,
-			feature: feature
-		} );
-	};
-
-	handleClickForTracking = ( type, feature ) => {
-		return () => this.trackProStatusClick( type, feature );
 	};
 
 	getProActions = ( type, feature ) => {
@@ -116,7 +128,7 @@ class ProStatus extends React.Component {
 					message
 				}
 				{
-					action && <NoticeAction onClick={ this.handleClickForTracking( type, feature ) } href={ actionUrl }>{ action }</NoticeAction>
+					action && <NoticeAction onClick={ handleClickForTracking( type, feature ) } href={ actionUrl }>{ action }</NoticeAction>
 				}
 			</SimpleNotice>
 		);
@@ -132,7 +144,7 @@ class ProStatus extends React.Component {
 	getSetUpButton = feature => {
 		return (
 			<Button
-				onClick={ this.handleClickForTracking( 'set_up', feature ) }
+				onClick={ handleClickForTracking( 'set_up', feature ) }
 				compact={ true }
 				primary={ true }
 				href={ `https://wordpress.com/plugins/setup/${ this.props.siteRawUrl }?only=${ feature }` }
@@ -159,69 +171,56 @@ class ProStatus extends React.Component {
 			hasScan = get( vpData, [ 'data', 'features', 'security' ], false );
 
 		const getStatus = ( feature, active, installed ) => {
-			if ( this.props.isDevMode ) {
-				return '';
-			}
+			switch ( feature ) {
+				case 'rewind':
+					return this.getProActions( 'rewind_connected', 'rewind' );
 
-			if ( 'rewind' === feature ) {
-				return this.getProActions( 'rewind_connected', 'rewind' );
-			}
-
-			if ( 'backups' === feature ) {
-				if ( hasFree && ! hasBackups ) {
-					if ( this.props.isCompact ) {
+				case 'backups':
+					if ( hasFree && ! hasBackups && this.props.isCompact ) {
 						return this.getProActions( 'free', 'backups' );
 					}
-				}
+					break;
+
+				case 'scan':
+					if ( this.props.fetchingSiteData || this.props.isFetchingVaultPressData ) {
+						return '';
+					}
+					if ( ( hasFree || hasPersonal ) && ! hasScan ) {
+						if ( this.props.isCompact ) {
+							return this.getProActions( 'free', 'scan' );
+						} else if ( hasPersonal && ! hasBackups ) {
+							// Personal plans doesn't have scan but it does have backups.
+							return this.getSetUpButton( 'backups' );
+						}
+						return '';
+					}
+					if ( 'N/A' !== vpData ) {
+						if ( ! hasScan ) {
+							return this.getSetUpButton( 'scan' );
+						}
+
+						return this.getProActions( 0 === this.props.getScanThreats() ? 'secure' : 'threats', 'scan' );
+					}
+					break;
+
+				case 'akismet':
+					if ( hasFree && ! ( active && installed ) ) {
+						return this.props.isCompact
+							? this.getProActions( 'free', 'anti-spam' )
+							: '';
+					}
+
+					if ( ! this.props.isAkismetKeyValid && ! this.props.fetchingAkismetData && active && installed ) {
+						return this.getProActions( 'invalid_key', 'anti-spam' );
+					}
+					break;
 			}
 
-			if ( 'scan' === feature ) {
-				if ( this.props.fetchingSiteData || this.props.isFetchingVaultPressData ) {
-					return '';
-				}
-				if ( ( hasFree || hasPersonal ) && ! hasScan ) {
-					if ( this.props.isCompact ) {
-						return this.getProActions( 'free', 'scan' );
-					} else if ( hasPersonal && ! hasBackups ) {
-						// Personal plans doesn't have scan but it does have backups.
-						return this.getSetUpButton( 'backups' );
-					}
-					return '';
-				}
-				if ( 'N/A' !== vpData ) {
-					if ( ! hasScan ) {
-						return this.getSetUpButton( 'scan' );
-					}
-
-					const threatsCount = this.props.getScanThreats();
-					if ( 0 !== threatsCount ) {
-						return this.getProActions( 'threats', 'scan' );
-					}
-					if ( 0 === threatsCount ) {
-						return this.getProActions( 'secure', 'scan' );
-					}
-				}
-			}
-
-			if ( 'akismet' === feature ) {
-				if ( hasFree && ! ( active && installed ) ) {
-					if ( this.props.isCompact ) {
-						return this.getProActions( 'free', 'anti-spam' );
-					}
-					return '';
-				}
-
-				if ( ! this.props.isAkismetKeyValid && ! this.props.fetchingAkismetData && active && installed ) {
-					return this.getProActions( 'invalid_key', 'anti-spam' );
-				}
-			}
 			// Show set up or active status only for paid features that depend on a plugin, and only under a pid plan
 			if ( sitePlan.product_slug && pluginSlug && ! hasFree ) {
-				if ( active && installed ) {
-					return this.getProActions( 'active' );
-				}
-
-				return this.getSetUpButton( feature );
+				return active && installed
+					? this.getProActions( 'active' )
+					: this.getSetUpButton( feature );
 			}
 
 			return '';
@@ -232,7 +231,7 @@ class ProStatus extends React.Component {
 				<QuerySitePlugins />
 				<QueryAkismetKeyCheck />
 				<QueryVaultPressData />
-				{ getStatus(
+				{ ! this.props.isDevMode && getStatus(
 					this.props.proFeature,
 					this.props.pluginActive( pluginSlug ),
 					this.props.pluginInstalled( pluginSlug )
